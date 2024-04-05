84ed97411a2b49860c73173d2df0c1e1b508a532
AndresRoches <maroches@eaglehubs.com>
2024-04-05 15:05:10 -0600

    Create project

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Jenkins_logo.svg/452px-Jenkins_logo.svg.png?20120629215426"} className="App-logo" alt="logo" />
+        <p>
+          Lab 4 levantado desde jenkins.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
